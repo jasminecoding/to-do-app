55c2468d79428c85cb0bc408ba1a777d9893db42
jasmine <jasminesneed01@gmail.com>
2021-03-06 23:17:45 +0900

    Our first commit

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ let sanitizeHTML = require('sanitize-html')
 let app = express()
 let db 
 
+let port = process.env.PORT
+if (port == null || port ==""){
+  port = 3000
+}
+
 app.use(express.static('public'))
 
 let connectionString = 'mongodb+srv://todoAppUser:Se@ttle1@cluster0.iod2u.mongodb.net/ToDoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
   db = client.db()
-  app.listen(3000)
+  app.listen(port)
 })
 
 app.use(express.json())
